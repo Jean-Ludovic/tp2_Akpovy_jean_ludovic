c68b9fa3b5461f9f21df5f5857061c9acbe995b7
Jean-Ludovic <ludotoro02@gmail.comm>
2023-11-18 14:57:18 -0500

    connect

--- config/connexion.php
+++ config/connexion.php
@@ -1,17 +1,39 @@
 <?php
 
-//  les informations de connexion de votre base de données
-$host = "localhost";
+//  les informations de connexion de la base de données
+$servername = "localhost";
 $username = "root";
 $password = "";
-$database = "ecom1_tp2";
-
-// Création de la connexion
-$conn = mysqli_connect($host, $username, $password, $database);
-
-// Vérification de la connexion
-if (!$conn) {
-    die("Erreur de connexion à la base de données : " . mysqli_connect_error());
+$dbname = "ecom1_tp2";
+try{
+    
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo"YOU ARE CONNECTED WITH ecom1_tp2 DATABASE";
 }
+catch(PDOException $e){
+    echo"La connexion a echoue".$e->getMessage();
+}
+if(isset($_POST['envoyer'])) {
+    $street = $_POST['street'];
+    $street_nb = $_POST['street_nb'];
+    $type = $_POST['type'];
+    $city = $_POST['city'];
+    $zipcode = $_POST['zipcode'];
+
+    $sql = "INSERT INTO `address` (`street`, `street_nb`, `type`, `city`, `zipcode`) VALUES (:street, :street_nb, :type, :city, :zipcode)";
+    
+    $stmt = $conn->prepare($sql);
+
+    $stmt->bindParam(':street', $street);
+    $stmt->bindParam(':street_nb', $street_nb);
+    $stmt->bindParam(':type', $type);
+    $stmt->bindParam(':city', $city);
+    $stmt->bindParam(':zipcode', $zipcode);
+
+    $stmt->execute();
+}
+
+
 
 ?>
