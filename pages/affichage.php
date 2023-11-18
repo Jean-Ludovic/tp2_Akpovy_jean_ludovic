b06523cb5a35894a45d5ba382d50d2390b779d87
Jean-Ludovic <ludotoro02@gmail.comm>
2023-11-18 14:58:07 -0500

    afficage.php

---
+++ pages/affichage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Merci pour votre visite <br><br>
+    <a href="confirmation.php">Aller à la page de confirmation</a><br><br>
+    <a href="index.php">Aller à la page d'acceuil</a>
+
+</body>
+</html>
