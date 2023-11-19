a3952023381a7c622aaae1b75526a5d19c5bad0c
Jean-Ludovic <ludotoro02@gmail.comm>
2023-11-18 19:00:01 -0500

    tt

--- config/functions.php
+++ config/functions.php
@@ -49,4 +49,48 @@ function displayFormDataAsTable($data_iterations) {
 
 // Récupérez les données depuis la base de données
 $data_iterations = getFormDataFromDatabase();
+// Supposez que $current_iteration contient les données actuelles du formulaire
+function afficherFormulaireAvecDonnees($current_iteration) {
+    // Afficher le formulaire pré-rempli avec les données existantes
+    echo '<form action="adresse.php?num_formulaires=' . $current_iteration['num_formulaires'] . '&current_iteration=' . $current_iteration['current_iteration'] . '" method="post">';
+    echo '<input type="hidden" name="num_formulaires" value="' . $current_iteration['num_formulaires'] . '">';
+    echo '<input type="hidden" name="current_iteration" value="' . $current_iteration['current_iteration'] . '">';
+
+    echo '<label for="street">Rue:</label>';
+    echo '<input type="text" id="street" name="street" maxlength="50" value="' . $current_iteration['street'] . '" required>';
+    echo '<br>';
+
+    echo'<label for="street_nb">Numéro de Rue:</label>
+    <input type="number" id="street_nb" name="street_nb" required>
+    <br>';
+    echo'label for="type">Type:</label>
+    <select id="type" name="type" maxlength="20" required>
+        <option value="livraison">Livraison</option>
+        <option value="facturation">Facturation</option>
+        <option value="autre">Autre</option>
+    </select>
+    <br>';
+    echo'<label for="city">Ville:</label>
+    <select id="city" name="city" required>
+        <option value="Montreal">Montréal</option>
+        <option value="Laval">Laval</option>
+        <option value="Toronto">Toronto</option>
+        <option value="Quebec">Québec</option>
+    </select>
+    <br>';
+    echo' <label for="zipcode">Code postal:</label>
+    <input type="text" id="zipcode" name="zipcode" pattern="[A-Za-z][0-9][A-Za-z] [0-9][A-Za-z][0-9]" maxlength="7" required>
+    <small>Format: A1A 1A1</small>
+    <br><br>
+    <input type="submit" value="Soumettre" name="envoyer" >
+</form>';
+    echo'';
+    
+
+
+
+    echo '<input type="submit" value="Soumettre" name="envoyer">';
+    echo '</form>';
+}
+
 ?>
